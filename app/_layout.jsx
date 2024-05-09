99f05dc790d71e96881b39d462e0e9a90232005d
hilmibaskara <92710770+hilmibaskara@users.noreply.github.com>
2024-05-09 17:17:39 +0700

    fix: remove header in tabs

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -25,6 +25,7 @@ export default function RootLayout() {
     return (
         <Stack>
             <Stack.Screen name="index" options={{headerShown: true}} />
+            <Stack.Screen name="(tabs)" options={{headerShown: false}} />
         </Stack>
     )
 }
